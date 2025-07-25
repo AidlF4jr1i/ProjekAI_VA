b08670fda03c6bf5e18de184774a443be48aebbb
AidlF4jr1i <ahmadaidilfajri0@gmail.com>
2025-07-25 17:53:06 +0700

    update learning(package)

---
+++ Learning Material/Advanced/module/playlist.py
@@ -0,0 +1,9 @@
+GENRE = "ANIME"
+DURATION = 'SHORT'
+TYPE = "CHEERFULL"
+
+def play(song):
+    print(f"memutar lagu {song}")
+
+def pause(song):
+    print(f"lagu '{song}' dihentikan")
---
+++ Learning Material/Advanced/package/music/playlist/load.py
@@ -0,0 +1,2 @@
+def load(song):
+    print(f"sedang memuat lagu {song} silahkan tunggu beberapa saat......" )
